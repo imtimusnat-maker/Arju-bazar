ac4b226f97135981bc6f04798f234043461c77a8
imtimusnat <imtimusnat@gmail.com>
2025-10-31 10:12:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1128361556-a7986",
+  "appId": "1:468806294491:web:fda42e827ce2a82824e5fe",
+  "apiKey": "AIzaSyDx8vyHRz0525OWYaSZcxgBCDzcOhrGfDY",
+  "authDomain": "studio-1128361556-a7986.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "468806294491"
+};
